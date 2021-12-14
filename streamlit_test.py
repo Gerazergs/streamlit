5638acbaffd5a070379c79879ced7535f3116fc9
Gerardo Garza <gerardo120386@gmail.com>
2021-12-14 16:10:01 -0600

    version prueba

--- streamlit_test.py
+++ streamlit_test.py
@@ -5,23 +5,23 @@ from numpy.random import default_rng
 import random
 
 
-# #CSS
-# def local_css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+#CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-# def remote_css(url):
-#     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
 
-# def icon(icon_name):
-#     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
-# local_css("style.css")
-# remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+local_css("style.css")
+remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-# #icon("search")
-# #selected = st.text_input("")
-# button_clicked = st.button("Re-load")
+#icon("search")
+#selected = st.text_input("")
+button_clicked = st.button("Re-load")
 
 
 
