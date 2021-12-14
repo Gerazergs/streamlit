b9168eb3300af95b1428f7a8bcb9f73ec5a731e7
Gerardo Garza <gerardo120386@gmail.com>
2021-12-14 16:07:49 -0600

    version prueba

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,64 +1,64 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from numpy.random import default_rng
-import random
+#import pandas as pd
+#import numpy as np
+#from numpy.random import default_rng
+#import random
 
 
-#CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# #CSS
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-def remote_css(url):
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+# def remote_css(url):
+#     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
 
-def icon(icon_name):
-    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+# def icon(icon_name):
+#     st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
-local_css("style.css")
-remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+# local_css("style.css")
+# remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
-#icon("search")
-#selected = st.text_input("")
-button_clicked = st.button("Re-load")
-
-
+# #icon("search")
+# #selected = st.text_input("")
+# button_clicked = st.button("Re-load")
 
 
-def categorical(palabras:int, listado:list):
+
+
+# def categorical(palabras:int, listado:list):
     
-    return listado[palabras]
+#     return listado[palabras]
     
 
-def create_random_number(numero_1, numero_2):
-    return random.randint(numero_1, numero_2)
+# def create_random_number(numero_1, numero_2):
+#     return random.randint(numero_1, numero_2)
 
-def create_random_number2(numero_1, numero_2):
-    size = numero_2
-    rng = default_rng(17)
-    return rng.normal(size=size)
+# def create_random_number2(numero_1, numero_2):
+#     size = numero_2
+#     rng = default_rng(17)
+#     return rng.normal(size=size)
 
 
 
-df = pd.DataFrame({
-    'llave_1': create_random_number2(17,100)
-    ,'llave_2': [create_random_number(1,7)*x + 100 for x in create_random_number2(17,100) ]
-    ,'llave_3': create_random_number2(70,100)
-    ,'llave_4': create_random_number2(80,100)    
-    ,'categorical':[categorical(create_random_number(0,1),['group_a','group_b','group_c']) for x in range(0,100)]
-    ,'cat_2':[categorical(create_random_number(0,4),['group_1','group_2','group_3','group_4','group_5']) for x in range(0,100)]
-    }
-)
-df = df.sort_values('cat_2', axis= 0)
+# df = pd.DataFrame({
+#     'llave_1': create_random_number2(17,100)
+#     ,'llave_2': [create_random_number(1,7)*x + 100 for x in create_random_number2(17,100) ]
+#     ,'llave_3': create_random_number2(70,100)
+#     ,'llave_4': create_random_number2(80,100)    
+#     ,'categorical':[categorical(create_random_number(0,1),['group_a','group_b','group_c']) for x in range(0,100)]
+#     ,'cat_2':[categorical(create_random_number(0,4),['group_1','group_2','group_3','group_4','group_5']) for x in range(0,100)]
+#     }
+# )
+# df = df.sort_values('cat_2', axis= 0)
 
 st.write('Mi first app with stramlit : just ploting random samples')
 
-st.table(df.head())
+# st.table(df.head())
 
-row2_spacer1, row2_spacer3 = st.columns(
-    (.1, .1)
-    )
+# row2_spacer1, row2_spacer3 = st.columns(
+#     (.1, .1)
+#     )
 
 
 
